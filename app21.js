56467193c220c48c4c2cf663b648c16f37c2e7f7
joseguilherme-zg <zansavio.goncalves@escola.pr.gov.br>
2025-11-07 11:56:54 -0300

    update

--- app21.js
+++ app21.js
@@ -3,6 +3,6 @@ const vini = document.getElementById('vini')
 vini.addEventListener("click",() => {
   const audio = document.createElement('audio');
   audio.src = 'hino.mp3';
-  audio.volume = 0.5; 
+  audio.volume = 1.0; 
   audio.play();
 })
